36a4adc203c15387940c7a7488288867b1f63c33
PJ <pcheynem1520@gmail.com>
2021-10-21 20:43:34 -0400

    uze module defined

--- upper_zero_extender.sv
+++ upper_zero_extender.sv
@@ -1,14 +1,15 @@
 module upper_zero_extender (
-    /* input signals */
-
     /* input buses */
-    
-    /* output signal */
+    input logic [15:0] lower_bits,
     
     /* output buses */
+    output logic [31:0] extended
     
 );
 
-    
+    always_comb begin : upper_bit_extension
+        extended[31:16] <= 16'b0000000000000000;    // upper 16 bits
+        extended[15:0] <= lower_bits[15:0];         // lower 16 bits
+    end
 
 endmodule
