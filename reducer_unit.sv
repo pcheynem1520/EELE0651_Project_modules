9e6156982ff0f693a72b92e1bf04daa7c78ba95e
PJ <pcheynem1520@gmail.com>
2021-10-21 23:32:09 -0400

    typecasting?

--- reducer_unit.sv
+++ reducer_unit.sv
@@ -10,4 +10,7 @@ module reducer_unit (
         data_out[7:0] <= data_in[7:0];  // assign lower 8 bits of input to output
     end
 
+    // suggested in project description, seems unnecessary
+    // data_out <= unsigned(data_out); // re-casting data_out to be unsigned
+
 endmodule
