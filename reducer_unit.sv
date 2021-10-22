3d474b0aae8920b6c775351ed690e62082044d6d
PJ <pcheynem1520@gmail.com>
2021-10-21 20:43:24 -0400

    reducer module defined

--- reducer_unit.sv
+++ reducer_unit.sv
@@ -1,14 +1,13 @@
 module reducer_unit (
-    /* input signals */
-
     /* input buses */
-    
-    /* output signal */
-    
+    input logic [31:0] data_in, // data input from instruction register
+
     /* output buses */
-    
+    output logic [7:0] data_out // data output from reducer unit
 );
 
-    
+    always_comb begin : reducing
+        data_out[7:0] <= data_in[7:0];  // assign lower 8 bits of input to output
+    end
 
 endmodule
