a1f387f262b22cd388d719151678397fcc280635
PJ <pcheynem1520@gmail.com>
2021-10-21 20:43:03 -0400

    lze module defined

--- lower_zero_extender.sv
+++ lower_zero_extender.sv
@@ -1,14 +1,14 @@
 module lower_zero_extender (
-    /* input signals */
-
     /* input buses */
-    
-    /* output signal */
-    
+    input logic [15:0] lower_bits,
+
     /* output buses */
-    
+    output logic [31:0] extended
 );
 
-    
+    always_comb begin : lower_bit_extension
+        extended[31:16] <= lower_bits[15:0];    // upper 16 bits
+        extended[15:0] <= 16'b0000000000000000; // lower 16 bits
+    end
 
 endmodule
